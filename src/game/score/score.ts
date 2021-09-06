a55010578221ded712aca38bdbeb1db20fa8cf9c
leonardo <leolub150@gmail.com>
2021-09-06 09:32:26 -0300

    refactor: ensure score uses separated function

--- src/game/score/score.ts
+++ src/game/score/score.ts
@@ -1,13 +1,17 @@
 import { Player } from '../types'
-import { scoreLookup } from './scoreLookup'
+import { Score, scoreLookup } from './scoreLookup'
 
 type ScoreParams = {
   player_1: Player;
   player_2: Player;
 }
 
+const getPoints = (player: Player) => scoreLookup(player.points)
+const asStrScore = (point_1: Score, point_2: Score) => `${point_1}-${point_2}`
+const makeScoreLookup = (p1: Player, p2: Player) => asStrScore(getPoints(p1), getPoints(p2))
+
 export function score ({ player_1, player_2 }: ScoreParams) {
   return {
-    str_score: `${scoreLookup(player_1.points)}-${scoreLookup(player_2.points)}`
+    str_score: makeScoreLookup(player_1, player_2)
   }
 }
